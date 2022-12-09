19ecb31f44ae8838954e9b082e2ed5f0c5a6df0a
scottBowles <shbowles@gmail.com>
2022-12-09 00:15:05 -0500

    Add login url & trailing slashes

--- src/lib/utils/urls.ts
+++ src/lib/utils/urls.ts
@@ -1,7 +1,8 @@
 export default {
+	login: () => '/login/',
 	challenge: (challengeSetId: number, challengeId: number) =>
-		`/challenge-set/${challengeSetId}/challenge/${challengeId}`,
+		`/challenge-set/${challengeSetId}/challenge/${challengeId}/`,
 	challengeSet: (id: number) => `/challenge-set/${id}/`,
-	challengeSetResults: (id: number) => `/challenge-set/${id}/results`,
-	challengeSetReview: (id: number) => `/challenge-set/${id}/review`
+	challengeSetResults: (id: number) => `/challenge-set/${id}/results/`,
+	challengeSetReview: (id: number) => `/challenge-set/${id}/review/`
 };
