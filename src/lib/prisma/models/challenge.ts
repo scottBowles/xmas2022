1382318716ffa1f1b32eb26d5e9a5ca8bfb34573
scottBowles <shbowles@gmail.com>
2022-12-09 21:21:05 -0500

    Add review page with most if not all the functionality from last year

--- src/lib/prisma/models/challenge.ts
+++ src/lib/prisma/models/challenge.ts
@@ -4,10 +4,66 @@ type IsLast = <T extends { challenges: { id: number }[] }>(
 	challengeSet: T
 ) => (challenge: T['challenges'][number]) => boolean;
 
+type CorrectAnswerFromOptionsMinimalInput = { options: { isCorrect: boolean; text: string }[] };
+type CorrectAnswerFromOptions = <T extends CorrectAnswerFromOptionsMinimalInput>(
+	challenge: T
+) => string | false | undefined;
+
+type CorrectAnswerFromAcceptedResponsesMinimalInput = {
+	acceptedResponsesIfOpen: string[];
+};
+type CorrectAnswerFromAcceptedResponses = <
+	T extends CorrectAnswerFromAcceptedResponsesMinimalInput
+>(
+	challenge: T
+) => string | false | undefined;
+
+type CorrectAnswerMinimalInput = CorrectAnswerFromOptionsMinimalInput &
+	CorrectAnswerFromAcceptedResponsesMinimalInput;
+type CorrectAnswer = <T extends CorrectAnswerMinimalInput>(
+	challenge: T
+) => string | false | undefined;
+
+type ResponseMinimalInput = { responses: { response: string }[] };
+type Response = <T extends ResponseMinimalInput>(challenge: T) => string;
+
+type ResponseIsCorrectMinimalInput = ResponseMinimalInput & CorrectAnswerMinimalInput;
+type ResponseIsCorrect = <T extends ResponseIsCorrectMinimalInput>(challenge: T) => boolean;
+
 const isLast: IsLast = (challengeSet) => (challenge) =>
 	challengeSet.challenges[challengeSet.challenges.length - 1].id === challenge.id;
 
+const correctAnswerFromOptions: CorrectAnswerFromOptions = (challenge) =>
+	challenge.options.find((option) => option.isCorrect)?.text;
+
+const correctAnswerFromAcceptedResponses: CorrectAnswerFromAcceptedResponses = (challenge) =>
+	challenge.acceptedResponsesIfOpen[0];
+
+const correctAnswer: CorrectAnswer = (challenge) =>
+	'options' in challenge && challenge.options.length > 0
+		? correctAnswerFromOptions(challenge)
+		: correctAnswerFromAcceptedResponses(challenge);
+
+const response: Response = (challenge) => challenge.responses[0]?.response;
+
+const responseIsCorrect: ResponseIsCorrect = (challenge) =>
+	response(challenge) === correctAnswer(challenge);
+
 const addIsLast = <T extends { challenges: { id: number }[] }>(challengeSet: T) =>
 	addKey('isLast', isLast(challengeSet));
+const addCorrectAnswer = addKey('correctAnswer', correctAnswer);
+const addResponse = addKey('response', response);
+const addResponseIsCorrect = addKey('responseIsCorrect', responseIsCorrect);
 
-export { isLast, addIsLast };
+export {
+	isLast,
+	correctAnswerFromOptions,
+	correctAnswerFromAcceptedResponses,
+	correctAnswer,
+	response,
+	responseIsCorrect,
+	addIsLast,
+	addCorrectAnswer,
+	addResponse,
+	addResponseIsCorrect
+};
--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -26,3 +26,29 @@ export const getNow = () => new Date();
 export const addKey =
 	<T extends Record<string, unknown>, K extends string, V>(key: K, transform: (obj: T) => V) =>
 	(obj: T): T & Record<K, V> => ({ ...obj, [key]: transform(obj) });
+
+export function capitalize(str: string) {
+	return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+export function formatDuration(ms: number, withUnits = true) {
+	const s = Math.floor(ms / 1000);
+	const m = Math.floor(s / 60);
+	const h = Math.floor(m / 60);
+	const sec = s % 60;
+	const min = m % 60;
+	const hours = h % 24;
+	const days = Math.floor(h / 24);
+	if (withUnits) {
+		const hoursStr = hours ? `${hours} hr ` : '';
+		const minStr = min ? `${min} min ` : '';
+		const secStr = sec + ' sec';
+		const daysStr = days ? `${days} day ` : '';
+		return daysStr + hoursStr + minStr + secStr;
+	}
+	const hoursStr = hours ? `${hours}:` : '';
+	const minStr = min ? `${min}:` : '';
+	const secStr = sec.toString().padStart(2, '0');
+	const daysStr = days ? `${days}:` : '';
+	return `${daysStr}${hoursStr}${minStr}${secStr}`;
+}
