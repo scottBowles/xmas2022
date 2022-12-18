eba30a4a352c02da6e940a7e071510d7ea96e9a6
scottBowles <shbowles@gmail.com>
2022-12-18 00:00:31 -0500

    Add +error.svelte

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,5 +1,17 @@
-<h1 class="text-xl text-christmasRed">Something went wrong</h1>
+<script>
+	import PageMargin from '$lib/components/PageMargin.svelte';
+</script>
 
-<p>Well that's not great. Let Scott know.</p>
+<PageMargin>
+	<div class="flex flex-col items-center mt-12">
+		<div>
+			<h1 class="text-xl text-christmasRed mt-4">Something went wrong</h1>
 
-<p><a href="/" class="text-green-700 hover:underline">Click here</a> to go to the home page.</p>
+			<p class="mt-4">Well that's not great. Let Scott know.</p>
+
+			<p class="mt-4">
+				<a href="/" class="text-green-700 hover:underline">Click here</a> to go to the home page.
+			</p>
+		</div>
+	</div>
+</PageMargin>
