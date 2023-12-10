c3343f6e70366c57aad53e51a9c88bccfb0260a9
scottBowles <shbowles@gmail.com>
2023-12-10 03:30:30 -0500

    Add normalize function to match.ts

--- src/lib/prisma/models/challenge/match.ts
+++ src/lib/prisma/models/challenge/match.ts
@@ -1,18 +1,19 @@
-import { correctAnswerFromAcceptedResponses, response } from './utils';
+import { correctAnswerFromAcceptedResponses, normalize, response } from './utils';
 import type { CorrectAnswer, ResponseIsCorrect, ScoreChallenge } from './types';
 
 const correctAnswer: CorrectAnswer = correctAnswerFromAcceptedResponses;
 
 const responseIsCorrect: ResponseIsCorrect = (challenge) =>
 	challenge.acceptedResponsesIfOpen.some(
-		(answer) => answer.toLowerCase() === response(challenge)?.toLowerCase()
+		(answer) => normalize(answer) === normalize(response(challenge))
 	);
 
 const scoreChallenge: ScoreChallenge = (challenge) => {
 	const answer = JSON.parse(correctAnswer(challenge) || '[]');
 	const res = JSON.parse(response(challenge) || '[]');
 	const numIndicesInCommon = answer.reduce(
-		(acc: number, val: string, index: number) => (val === res[index] ? acc + 1 : acc),
+		(acc: number, val: string, index: number) =>
+			normalize(val) === normalize(res[index]) ? acc + 1 : acc,
 		0
 	);
 	return Math.round((numIndicesInCommon / answer.length) * challenge.points);
