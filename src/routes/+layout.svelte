1f9cc7006453feef6a94b7d8daba717773109f2a
scottBowles <shbowles@gmail.com>
2023-12-10 02:16:40 -0500

    Add @vercel/speed-insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,9 @@
 	import '../app.css';
 	import Nav from '$lib/components/Nav.svelte';
 	import ToastContainer from '$lib/wordle/components/Toast/Container.svelte';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
+	injectSpeedInsights();
 
 	export let data;
 </script>
