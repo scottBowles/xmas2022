b3e47d5877f6cb5e13eb6456e42f5b8a56867c9c
scottBowles <shbowles@gmail.com>
2023-12-10 02:11:49 -0500

    Add match challenge type code

---
+++ src/routes/(authenticated)/challenge-set/[setId]/challenge/[challengeId]/MatchForm.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import type { PageData } from './$types';
+	import { NEXT_INPUT_VALUE, SUBMIT_INPUT_VALUE } from './constants';
+
+	export let data: PageData;
+
+	$: ({ challenge, setHasAnotherChallenge } = data);
+	$: response = JSON.parse(challenge?.responses[0]?.response ?? '[]');
+
+	let values: string[] = [];
+	$: {
+		if (values.length === 0 && challenge?.matches?.length > 0) {
+			values = challenge?.matches?.map((_, i) => response[i] ?? '') ?? [];
+		}
+	}
+	$: json = JSON.stringify(values);
+</script>
+
+<form class="flex flex-col justify-between grow sm:justify-start" method="POST" use:enhance>
+	<legend class="mb-4 text-lg">{@html challenge.prompt}</legend>
+
+	<ol class="mb-6">
+		{#each challenge.matches as match, i}
+			<li class="flex flex-col sm:flex-row sm:items-center sm:space-x-4">
+				<p>{i + 1}. {match}</p>
+			</li>{/each}
+	</ol>
+
+	<ol class="mb-6">
+		{#each challenge.matchOptions as matchOption, i}
+			<li class="flex flex-col sm:flex-row sm:items-center sm:space-x-4">
+				<!-- as above, but use lowercase letters instead of numbers for enumerating (a., b., c. instead of 1., 2., 3.) -->
+				<p>{String.fromCharCode(97 + i)}. {matchOption}</p>
+			</li>{/each}
+	</ol>
+
+	<input type="hidden" name="answer" value={json} />
+	<p>In each box, put only the letter that matches with the number above.</p>
+	<div class="flex flex-wrap gap-6">
+		{#each values as _, i}
+			<label class="flex flex-col sm:flex-row sm:items-center sm:space-x-2">
+				<span>{i + 1}</span>
+				<input
+					type="text"
+					name={`match_${i}`}
+					class="border border-gray-300 rounded py-2 px-4 w-16"
+					bind:value={values[i]}
+					autofocus={i === 0}
+					required
+				/>
+			</label>
+		{/each}
+	</div>
+
+	<input
+		type="submit"
+		value={setHasAnotherChallenge ? NEXT_INPUT_VALUE : SUBMIT_INPUT_VALUE}
+		name="submit_action"
+		class="bg-green-700 text-white font-bold py-3 px-6 rounded w-full text-lg my-8 block"
+	/>
+</form>
---
+++ src/routes/(authenticated)/challenge-set/[setId]/review/MatchReview.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	import * as match from '$lib/prisma/models/challenge/match';
+
+	export let challenge: PageData['challenges'][number];
+	$: correctValues = JSON.parse(challenge.correctAnswer || '[]');
+	$: givenValues = JSON.parse(challenge.responses[0]?.response || '[]');
+	$: points = match.scoreChallenge(challenge, { elfNameChallengeResponse: null });
+</script>
+
+<form class="flex flex-col justify-between grow sm:justify-start">
+	<legend class="mb-4 text-lg">{@html challenge.prompt}</legend>
+
+	<ol class="mb-6">
+		{#each challenge.matches as match, i}
+			<li class="flex flex-col sm:flex-row sm:items-center sm:space-x-4">
+				<p>{i + 1}. {match}</p>
+			</li>{/each}
+	</ol>
+
+	<ol class="mb-6">
+		{#each challenge.matchOptions as matchOption, i}
+			<li class="flex flex-col sm:flex-row sm:items-center sm:space-x-4">
+				<!-- as above, but use lowercase letters instead of numbers for enumerating (a., b., c. instead of 1., 2., 3.) -->
+				<p>{String.fromCharCode(97 + i)}. {matchOption}</p>
+			</li>{/each}
+	</ol>
+
+	<p class="mb-2">Your answers were:</p>
+	<div class="flex flex-wrap gap-6 mb-2">
+		{#each givenValues as value, i}
+			<label class="flex flex-col sm:flex-row sm:items-center sm:space-x-2">
+				<span>{i + 1}</span>
+				<input
+					type="text"
+					name={`match_${i}`}
+					class="border border-gray-300 rounded py-2 px-4 w-16"
+					{value}
+					disabled
+				/>
+			</label>
+		{/each}
+	</div>
+
+	{#if !challenge.responseIsCorrect}
+		<p class="mb-2">The correct answers were:</p>
+		<div class="flex flex-wrap gap-6 mb-2">
+			{#each correctValues as value, i}
+				<label class="flex flex-col sm:flex-row sm:items-center sm:space-x-2">
+					<span>{i + 1}</span>
+					<input
+						type="text"
+						name={`match_${i}`}
+						class="border border-gray-300 rounded py-2 px-4 w-16"
+						{value}
+						disabled
+					/>
+				</label>
+			{/each}
+		</div>
+	{/if}
+</form>
+
+<p class="my-2 text-green-700">
+	{points} / {challenge.points} points
+</p>
