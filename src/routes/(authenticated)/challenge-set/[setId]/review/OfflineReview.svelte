bb83cb6b02a8597f7e4f1ccf3dcee2b3e9b93b7c
scottBowles <shbowles@gmail.com>
2023-12-10 02:11:58 -0500

    Fix offline review

--- src/routes/(authenticated)/challenge-set/[setId]/review/OfflineReview.svelte
+++ src/routes/(authenticated)/challenge-set/[setId]/review/OfflineReview.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
 	import type { PageData } from './$types';
+	import * as offline from '$lib/prisma/models/challenge/offline';
 
 	export let challenge: PageData['challenges'][number];
-	$: ({ pointsEarned } = challenge.responses[0] || {});
+	$: points = offline.scoreChallenge(challenge, { elfNameChallengeResponse: null });
 </script>
 
 <p class="mb-3 text-lg">{@html challenge.prompt}</p>
-{#if pointsEarned}
+{#if points}
 	<p class="mb-4 text-lg text-green-700 italic">
-		You received {pointsEarned} points!
+		You received {points} points!
 	</p>
 {:else}
 	<p class="mb-4 text-lg italic">You haven't received points for this challenge</p>
