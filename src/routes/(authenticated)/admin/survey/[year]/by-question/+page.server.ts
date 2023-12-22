51b9da8b95794fe01106375837f701a88b8f2b82
scottBowles <shbowles@gmail.com>
2023-12-21 23:17:10 -0500

    Implement admin survey by-question

---
+++ src/routes/(authenticated)/admin/survey/[year]/by-question/+page.server.ts
@@ -0,0 +1,38 @@
+import prisma from '$lib/prisma';
+import { error } from '@sveltejs/kit';
+
+export const load = async ({ parent, params }) => {
+	const { user } = await parent();
+
+	if (!user.isAdmin) {
+		throw error(403, 'Forbidden');
+	}
+
+	const year = parseInt(params.year);
+
+	const surveyChallengeSet = await prisma.challengeSet.findFirst({
+		where: {
+			timeAvailableStart: {
+				gte: new Date(year, 0, 1),
+				lt: new Date(year + 1, 0, 1),
+			},
+			title: {
+				contains: 'survey',
+				mode: 'insensitive',
+			},
+		},
+		include: {
+			challenges: {
+				include: {
+					responses: {
+						include: {
+							player: true,
+						},
+					},
+				},
+			},
+		},
+	});
+
+	return { surveyChallengeSet };
+};
