ab07d9c6ad4530c9e7a940678e6bdbba0abebed1
scottBowles <shbowles@gmail.com>
2024-12-14 12:53:07 -0500

    Improve error page with status

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,13 +1,24 @@
 <script>
+	import { page } from '$app/stores';
 	import PageMargin from '$lib/components/PageMargin.svelte';
+
+	let { status } = $page;
+
+	let text = $derived(
+		(() => {
+			if (status === 404) return 'Page not found';
+			if (status === 500) return 'Server error';
+			return 'Something went wrong';
+		})()
+	);
 </script>
 
 <PageMargin>
 	<div class="flex flex-col items-center mt-12">
 		<div>
-			<h1 class="text-xl text-christmasRed mt-4">Something went wrong</h1>
+			<h1 class="text-xl text-christmasRed mt-4">{status}: {text}</h1>
 
-			<p class="mt-4">Well that's not great. If this was unexpected, let Scott know.</p>
+			<p class="mt-4">Sorry about that. If this was unexpected, let Scott know.</p>
 
 			<p class="mt-4">
 				<a href="/" class="text-green-700 hover:underline" data-sveltekit-preload-data="hover">
