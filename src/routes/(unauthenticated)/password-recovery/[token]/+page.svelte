38f12c3d648ab5e74288bdcdb1a4a19357466442
scottBowles <shbowles@gmail.com>
2024-12-16 17:49:51 -0500

    Password reset form styles

--- src/routes/(unauthenticated)/password-recovery/[token]/+page.svelte
+++ src/routes/(unauthenticated)/password-recovery/[token]/+page.svelte
@@ -6,20 +6,25 @@
 
 <form method="POST" class="flex flex-col items-center mt-8" use:enhance>
 	<fieldset class="flex flex-col items-center">
+		<legend class="text-4xl mb-1 text-green-700">Reset Password</legend>
 		<input
-			class="w-64 m-1 p-2 text-green-700 border border-green-700 rounded"
 			type="password"
 			name="password"
 			placeholder="New password"
 			required
+			class="w-64 m-1 p-2 text-green-700 border border-green-700 rounded"
 		/>
 		<input
-			class="w-64 m-1 p-2 text-green-700 border border-green-700 rounded"
 			type="password"
 			name="password_confirmation"
 			placeholder="Confirm new password"
 			required
+			class="w-64 m-1 p-2 text-green-700 border border-green-700 rounded"
 		/>
 	</fieldset>
-	<button type="submit">Reset password</button>
+	<input
+		type="submit"
+		value="Reset password"
+		class="w-64 m-1 p-2 border rounded bg-green-700 text-white text-lg cursor-pointer hover:bg-green-800"
+	/>
 </form>
