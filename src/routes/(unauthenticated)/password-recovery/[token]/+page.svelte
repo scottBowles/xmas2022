60f9b2b659ede2ec86752c17597e27b08af356f2
scottBowles <shbowles@gmail.com>
2024-12-16 08:55:20 -0500

    pw reset

--- src/routes/(unauthenticated)/password-recovery/[token]/+page.svelte
+++ src/routes/(unauthenticated)/password-recovery/[token]/+page.svelte
@@ -1,12 +1,25 @@
 <script lang="ts">
+	import { enhance } from '$app/forms';
+
 	let { form } = $props();
 </script>
 
-<form method="POST">
-	<input type="password" name="password" placeholder="New password" required />
-	<input type="password" name="password_confirmation" placeholder="Confirm new password" required />
+<form method="POST" class="flex flex-col items-center mt-8" use:enhance>
+	<fieldset class="flex flex-col items-center">
+		<input
+			class="w-64 m-1 p-2 text-green-700 border border-green-700 rounded"
+			type="password"
+			name="password"
+			placeholder="New password"
+			required
+		/>
+		<input
+			class="w-64 m-1 p-2 text-green-700 border border-green-700 rounded"
+			type="password"
+			name="password_confirmation"
+			placeholder="Confirm new password"
+			required
+		/>
+	</fieldset>
 	<button type="submit">Reset password</button>
-	{#if form.error}
-		<p>{form.error}</p>
-	{/if}
 </form>
