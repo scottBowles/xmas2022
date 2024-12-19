b946369db486213e126faf4d1e3d1b0799e6d54c
scottBowles <shbowles@gmail.com>
2024-12-18 23:16:38 -0500

    Improve match review

--- src/routes/(authenticated)/challenge-set/[setId]/review/MatchReview.svelte
+++ src/routes/(authenticated)/challenge-set/[setId]/review/MatchReview.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	import { match } from '$lib/prisma/models/challenge/challengeTypeFns';
+	import { normalize } from '@/prisma/models/challenge/utils';
 
 	interface Props {
 		challenge: PageData['challenges'][number];
@@ -10,6 +11,9 @@
 	let correctValues = $derived(JSON.parse(challenge.correctAnswer || '[]'));
 	let givenValues = $derived(JSON.parse(challenge.responses[0]?.response || '[]'));
 	let points = $derived(match.scoreChallenge(challenge, { elfNameChallengeResponse: null }));
+	let correctIndeces = $derived(
+		correctValues.map(normalize).map((v: string, i: number) => normalize(givenValues[i]) === v)
+	);
 </script>
 
 <form class="flex flex-col justify-between grow sm:justify-start">
@@ -18,7 +22,7 @@
 
 	<ol class="mb-6">
 		{#each challenge.matches as match, i}
-			<li class="flex flex-col sm:flex-row sm:items-center sm:space-x-4">
+			<li class={`flex flex-col sm:flex-row sm:items-center sm:space-x-4`}>
 				<p>{i + 1}. {match}</p>
 			</li>{/each}
 	</ol>
@@ -31,37 +35,47 @@
 			</li>{/each}
 	</ol>
 
-	<p class="mb-2">Your answers were:</p>
-	<div class="flex flex-wrap gap-6 mb-2">
-		{#each givenValues as value, i}
-			<label class="flex flex-col sm:flex-row sm:items-center sm:space-x-2">
-				<span>{i + 1}</span>
-				<input
-					type="text"
-					name={`match_${i}`}
-					class="border border-gray-300 rounded py-2 px-4 w-16"
-					{value}
-					disabled
-				/>
-			</label>
-		{/each}
-	</div>
-
-	{#if !challenge.responseIsCorrect}
-		<p class="mb-2">The correct answers were:</p>
+	<div class="mb-6">
+		<p class="mb-2">Your answers were:</p>
 		<div class="flex flex-wrap gap-6 mb-2">
-			{#each correctValues as value, i}
+			{#each givenValues as value, i}
 				<label class="flex flex-col sm:flex-row sm:items-center sm:space-x-2">
 					<span>{i + 1}</span>
 					<input
 						type="text"
 						name={`match_${i}`}
-						class="border border-gray-300 rounded py-2 px-4 w-16"
+						class={`border border-gray-300 rounded py-2 px-4 w-16 ${
+							correctIndeces[i] ? 'border-green-700 bg-green-100' : 'border-christmasRed bg-red-100'
+						}`}
 						{value}
 						disabled
 					/>
 				</label>
 			{/each}
+		</div>
+	</div>
+
+	{#if !challenge.responseIsCorrect}
+		<div>
+			<p class="mb-2">The correct answers were:</p>
+			<div class="flex flex-wrap gap-6 mb-2">
+				{#each correctValues as value, i}
+					<label class="flex flex-col sm:flex-row sm:items-center sm:space-x-2">
+						<span>{i + 1}</span>
+						<input
+							type="text"
+							name={`match_${i}`}
+							class={`border border-gray-300 rounded py-2 px-4 w-16 ${
+								correctIndeces[i]
+									? 'border-green-700 bg-green-100'
+									: 'border-christmasRed bg-red-100'
+							}`}
+							value={value.toLowerCase()}
+							disabled
+						/>
+					</label>
+				{/each}
+			</div>
 		</div>
 	{/if}
 </form>
